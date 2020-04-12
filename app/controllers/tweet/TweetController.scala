85f5beb615cb9bf7cc949b3e686c24647bb12419
taretmch <tomochuri25@gmail.com>
2020-04-12 22:36:14 +0900

    Implement tweet detail page

--- app/controllers/tweet/TweetController.scala
+++ app/controllers/tweet/TweetController.scala
@@ -6,9 +6,17 @@ import models.Tweet
 
 @Singleton
 class TweetController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
+  
+  val tweets: Seq[Tweet] = (1L to 10L).map(i => Tweet(Some(i), s"test tweet${i.toString}"))
 
   def list() = Action { implicit request: Request[AnyContent] =>
-    val tweets: Seq[Tweet] = (1L to 10L).map(i => Tweet(Some(i), s"test tweet${i.toString}"))
     Ok(views.html.tweet.list(tweets))
+  }
+
+  def show(id: Long) = Action { implicit request: Request[AnyContent] =>
+    val tweet = tweets.find(_.id.get == id) match {
+      case Some(t) => t
+    }
+    Ok(views.html.tweet.show(tweet))
   }
 }
